07598c1ae6fc434068d26ab7704f93bea5d387a3
Noah <mcbeasty11.nh@icloud.com>
2021-02-13 16:45:33 -0600

    var and console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("Ths is an alert! JavaScript is Running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(150 + 150);
+// what is this?
+console.log("Your robot " + playerName + ", has won!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight ();
