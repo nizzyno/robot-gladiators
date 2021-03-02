2b2915f21fe6eca8f4d5be0c700c123de820839c
Noah <mcbeasty11.nh@icloud.com>
2021-03-02 15:25:23 -0600

    fixed shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,17 +32,6 @@ var startGame = function () {
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
-
-            // if player is still alive and we're not at the last enemy in the array
-            if (playerHealth > 0 && i < enemyNames.length - 1) {
-                // ask if player wants to use the store before next round
-                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
-
-                // if yes, take them to the store() function
-                if (storeConfirm) {
-                    shop();
-                }
-            }
         }
         // if player is not alive, break out of the loop and let endGame function run
         else {
